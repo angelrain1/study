c7c6ef65f4fb45353b11d9bc0935f9a5c0d97bc5
angelrain1 <lagudomeze@163.com>
2017-08-31 17:44:43 +0800

    add generator study and add crossbeam comment

--- src/bin/context.rs
+++ src/bin/context.rs
@@ -1,4 +1,78 @@
+#![feature(proc_macro, conservative_impl_trait, generators, generator_trait)]
+
+fn returns_closure() -> impl FnMut(i32) -> i32 {
+    let mut a = [0; 4];
+    move |x| {
+        a[x as usize] += 1;
+        x + a[x as usize]
+    }
+}
+
+extern crate futures_await as futures;
+
+use futures::prelude::*;
+
+#[async]
+fn foo() -> Result<i32, i32> {
+    Ok(1 + await!(bar())?)
+}
+
+#[async]
+fn bar() -> Result<i32, i32> {
+    Ok(2)
+}
+
+use std::ops::Generator;
+
+fn yield_during_range_iter() {
+    // Should be OK.
+    let mut b = || {
+        let a = 0u32;
+        yield a;
+        println!("haha");
+        yield a;
+        println!("haha");
+        yield a;
+        println!("haha");
+        yield a;
+        println!("haha");
+        yield a;
+        println!("haha");
+        yield a;
+        println!("haha");
+        yield a;
+        println!("haha");
+        yield a;
+        println!("haha");
+        yield a;
+        println!("haha");
+        yield a;
+    };
+    println!("size of vec:{}", std::mem::size_of::<Vec<i32>>());
+    println!("size of vec:{}", std::mem::size_of::<std::slice::Iter<i32>>());
+    println!("size of gen:{}", std::mem::size_of_val(&b));
+    println!("{:?}", b.resume());
+    println!("{:?}", b.resume());
+    println!("{:?}", b.resume());
+    println!("{:?}", b.resume());
+}
+
 fn main() {
+    yield_during_range_iter();
+
+    let r = foo();
+    assert_eq!(r.wait(), Ok(3));
+
+    let mut f = returns_closure();
+
+    println!("size:{}", std::mem::size_of_val(&f));
+
+    println!("{}", f(0));
+    println!("{}", f(1));
+    println!("{}", f(2));
+    println!("{}", f(3));
+
+
     let mut a = 1u32;
     let mut b = [1u8; 100];
     println!("read b[0]:{}", b[0]);
@@ -20,7 +94,6 @@ fn main() {
         println!("a:{}", f());
         println!("a:{}", f());
         println!("a:{}", f());
-
     }
 
     println!("a:{}", a);
--- src/bin/crossbeam_study.rs
+++ src/bin/crossbeam_study.rs
@@ -11,7 +11,7 @@ fn main() {
     crossbeam::scope(|scope| {
         let (tx, rx) = channel();
 
-        // mutable borrow occurs here until scope end, this is mean submit a work to the worker/thread
+        // immutable borrow occurs here until scope end, this is mean submit a work to the worker/thread
         tx.send(&a).expect("");
 
         //spawn a scoped worker/thread
@@ -32,8 +32,34 @@ fn main() {
         for _ in 0..21 {
             println!("outer:do something");
             std::thread::sleep(std::time::Duration::from_millis(50));
+            // immutable borrow occurs here until scope end, this is mean submit a work to the worker/thread
             println!("outer:{:p},{}", &a, a);
         }
+
+        j.join();
+    });
+
+    crossbeam::scope(|scope| {
+        let (tx, rx) = channel();
+
+        // mutable borrow occurs here until scope end, this is mean submit a work to the worker/thread
+        tx.send(&mut a).expect("");
+
+        //spawn a scoped worker/thread
+        let j = scope.spawn(move || {
+            //                let ref_a: &mut u32 = &mut a;
+            let ref_a = rx.recv().expect("");
+            std::thread::sleep(std::time::Duration::from_millis(250));
+            println!("inner:{:p},{}", ref_a, *ref_a);
+            std::thread::sleep(std::time::Duration::from_millis(250));
+            //            *ref_a = 2;
+            std::thread::sleep(std::time::Duration::from_millis(250));
+            println!("inner:{:p},{}", ref_a, *ref_a);
+            std::thread::sleep(std::time::Duration::from_millis(250));
+        });
+
+        // immutable borrow occurs here mean is error
+        // println!("outer:{:p},{}", &a, a);
 
         j.join();
     });
