dd45bd7d8dbef515a50ba2cce0df4d5e41acb55d
angelrain1 <lagudomeze@163.com>
2017-09-07 16:02:44 +0800

    add structopt and log

---
+++ src/bin/fancify.rs
@@ -0,0 +1,53 @@
+#[macro_use]
+extern crate log;
+extern crate loggerv;
+
+#[macro_use]
+extern crate error_chain;
+
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use structopt::StructOpt;
+
+/// Do fancy things
+#[derive(StructOpt, Debug)]
+#[structopt(name = "fancify")]
+struct Cli {
+    /// Enable logging, use multiple `v`s to increase verbosity
+    #[structopt(short = "v", long = "verbose")]
+    verbosity: u64,
+}
+
+fn main() {
+    let args = Cli::from_args();
+    loggerv::init_with_verbosity(args.verbosity).expect("");
+
+    let thing = "foobar";
+
+    debug!("Thing happened: {}", thing);
+
+    info!("It's all good!");
+}
+
+//quick_main!(|| -> Result<()> {
+//    let args = Cli::from_args();
+//
+//    loggerv::init_with_verbosity(args.verbosity)?;
+//
+//    // ...
+//    let thing = "foobar";
+//    debug!("Thing happened: {}", thing);
+//    // ...
+//
+//    info!("It's all good!");
+//    Ok(())
+//});
+//
+//error_chain! {
+//    foreign_links {
+//        Log(::log::SetLoggerError);
+//    }
+//}
+
