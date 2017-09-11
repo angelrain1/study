24f03e4642dac63a112b04ff6c56c454e8bdefc9
angelrain1 <lagudomeze@163.com>
2017-09-11 10:21:05 +0800

    test for Sized marker: can be used for compile time assert

--- src/bin/coerce_study.rs
+++ src/bin/coerce_study.rs
@@ -1,6 +1,6 @@
 trait Trait {}
 
-fn foo<X: Trait>(t: X) {}
+fn foo<X: Trait>(_: X) {}
 
 impl<'a> Trait for &'a i32 {}
 
@@ -26,7 +26,27 @@ impl Foo for Baz {
 //    fn f(&self) { println!("Baz's impl"); }
 //}
 
+const CONSTANT_STRING: &'static str = "This is a constant string";
+
+const CONSTANT_ARRAY: &'static [u8] = &[0u8; 128];
+
+fn assert_same_type<'a, 'b, T: ?Sized>(_: &'a T, _: &'b T) {}
+
 fn main() {
+    let another_string = "This string is local to the main function";
+
+    assert_same_type(CONSTANT_STRING, another_string);
+
+    println!("size of constant string:{}", std::mem::size_of_val(CONSTANT_STRING));
+    println!("size of another  string:{}", std::mem::size_of_val(another_string));
+
+    let another_array = &[1u8; 64];
+
+    println!("size of constant array:{}", std::mem::size_of_val(CONSTANT_ARRAY));
+    println!("size of another  array:{}", std::mem::size_of_val(another_array));
+
+    assert_same_type(CONSTANT_ARRAY, another_array);
+
     let t: &mut i32 = &mut 0;
     foo(t as &i32);
 
